0d51ea46f1490e9761fa8e637e92898774dfc2e9
Samuel Gratzl <sam@sgratzl.com>
2020-11-12 10:21:59 -0500

    feat: simple function

--- api/get-to-post.js
+++ api/get-to-post.js
@@ -1,7 +1,17 @@
-module.exports = (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+    if (!req.query.url) {
+        throw new Error(`missing "url" parameter`);
+    }
+    const response = await fetch(req.query.url, {
+        method: 'POST',
+        body: req.query.body
     });
+
+    if (response.ok) {
+        return response.body.pipe(res);
+    }
+
+    throw new Error(`unexpected response ${response.statusText}`);
 }
