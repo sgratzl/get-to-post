cd48f025604a753c2c05671a37890f835cbdb00c
Samuel Gratzl <sam@sgratzl.com>
2021-09-03 10:22:19 -0400

    feat: add get-to-get and enable cors

---
+++ api/get-to-get.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+    if (!req.query.url) {
+        res.json({ status: 'error', message: `missing "url" parameter` });
+        return;
+    }
+    const response = await fetch(req.query.url, {
+        method: 'GET',
+    });
+
+    if (response.ok) {
+        return response.body.pipe(res);
+    }
+
+    res.json({ status: 'error', message: `unexpected response ${response.statusText}` });
+}
